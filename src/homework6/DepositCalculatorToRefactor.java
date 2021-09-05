0d57833d517c23d6c7749ad0334cf2d95dc7154f
Daniil Gatman <daniilhatman@gmail.com>
2021-09-05 16:43:40 +0300

    Completed HW6.    *The problem with displaying correct word was solved.    *Sums are now displaying rounded to 2 digits after the separator.

---
+++ src/homework6/DepositCalculatorToRefactor.java
@@ -0,0 +1,25 @@
+package homework6;
+
+import java.util.Scanner;
+
+public class DepositCalculatorToRefactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите сумму вклада в банк в гривнах: ");
+        double sum = sc.nextDouble();
+        System.out.print("Какой процент годовых? ");
+        double percent = sc.nextDouble();
+        System.out.print("Длительность вклада (лет): ");
+        int term = sc.nextInt();
+        System.out.println();
+
+        for (int i = 1; i <= term; i++) {
+            double y = sum / 100 * percent;
+            sum = sum + y;
+            System.out.println("Накопленная сумма за " + i + (i % 10 == 1 ? " год" :
+                    i % 10 == 2 || i % 10 == 3 || i % 10 == 4 ? " года"
+                            : " лет") + " составила: " + String.format("%.2f",sum));
+            System.out.println("Начисленные проценты в грн: " + String.format("%.2f",y));
+        }
+    }
+}
